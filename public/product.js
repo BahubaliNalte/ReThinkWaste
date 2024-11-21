8367ad074247b539ac140c75dc72db0c7a4d378d
BahubaliNalte <bahubalidnalte722006@gmail.com>
2024-11-21 13:20:58 +0530

    add cart

--- public/product.js
+++ public/product.js
@@ -3,7 +3,13 @@ const products = [
     { id: 1, name: 'Product 1', price: 10, image: 'product1.jpg' },
     { id: 2, name: 'Product 2', price: 20, image: 'product2.jpg' },
     { id: 3, name: 'Product 3', price: 30, image: 'product3.jpg' },
-    { id: 4, name: 'Product 4', price: 40, image: 'product4.jpg' }
+    { id: 4, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 5, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 6, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 7, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 8, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 9, name: 'Product 4', price: 40, image: 'product4.jpg' },
+    { id: 10, name: 'Product 4', price: 40, image: 'product4.jpg' },
 ];
 
 // Cart array to store items
