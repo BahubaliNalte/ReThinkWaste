bdf1d838106bfcf4324c42765010a105ddc00962
Bahubali Nalte <142397022+BahubaliNalte@users.noreply.github.com>
2025-11-02 21:01:04 +0530

    deployment error solve

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.urlencoded({ extended: true })); // To handle form data
 app.use(express.json()); // For JSON handling
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(helmet()); // Add security headers
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.set("view engine", "ejs");
